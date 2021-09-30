6255659cc152b208074fea70e6279a044de2cb44
vivekrmore <91602267+vivekrmore@users.noreply.github.com>
2021-09-30 17:05:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,4 +8,4 @@ app.get("/", (req, res)=>{
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 app.listen(9000);
-console.log("Website is being hosted on port no 9000");
+console.log("Website is being hosted on port no 9100");
